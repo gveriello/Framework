5a7cd59e145d70c59f8c03c898b38d666d82457b
Giuseppe Veriello <gveriello@outlook.it>
2018-09-23 08:10:46 +0200

    SqlHelper expanded addWhereCondition

--- core/utilities/helpers/SqlHelper.php
+++ core/utilities/helpers/SqlHelper.php
@@ -78,7 +78,38 @@ class SqlHelper
         if (empty($property) && empty($whereCondition))
             return false;
 
-        array_push(self::$whereConditions, implode(' ', array($property, $whereCondition, $value, $finalKey)));
+        $whereTemp = array();
+
+        if (empty($whereCondition) || empty($property))
+            return false;
+
+        array_push($whereTemp, $property, $whereCondition);
+
+        if ($whereCondition === WHERE_KEY::EQUAL ||
+            $whereCondition === WHERE_KEY::NOT_EQUAL ||
+            $whereCondition === WHERE_KEY::GREATER_THAN ||
+            $whereCondition === WHERE_KEY::LESS_THAN ||
+            $whereCondition === WHERE_KEY::GREATER_THAN_OR_EQUAL ||
+            $whereCondition === WHERE_KEY::LESS_THAN_OR_EQUAL)
+            array_push($whereTemp,  $value);
+
+        if ($whereCondition === WHERE_KEY::BETWEEN || $whereCondition === WHERE_KEY::NOT_BETWEEN)
+            if (is_array($value))
+                if (count($value) === 2)
+                    array_push($whereTemp, $value[0], OPERATOR_KEY::AND_KEY, $value[1]);
+                else 
+                    return false;
+
+        if ($whereCondition === WHERE_KEY::IN || $whereCondition === WHERE_KEY::NOT_IN)
+            if (is_array($value))
+                array_push($whereTemp, '(', implode(',', $value), ')');
+            else
+                return false;
+
+        array_push($whereTemp, $finalKey);
+
+        self::$whereConditions .= implode(' ', $whereTemp);
+        unset($whereCondition);
         return true;
     }
     #endregion
@@ -119,35 +150,3 @@ class SqlHelper
     }
     #endregion
 }
-//private function addWhereCondition($field, WHERE_KEY $key, $comparator, OPERATOR_KEY $operator = NULL)
-//{
-//    $whereTemp = array();
-
-//    if ($key === NULL || empty($field) || empty($comparator))
-//        return false;
-
-//    if ($key !== NULL)
-//        array_push($whereTemp, $field, $key);
-
-//    if ($key === WHERE_KEY::EQUAL ||
-//        $key === WHERE_KEY::NOT_EQUAL ||
-//        $key === WHERE_KEY::GREATER_THAN ||
-//        $key === WHERE_KEY::LESS_THAN ||
-//        $key === WHERE_KEY::GREATER_THAN_OR_EQUAL ||
-//        $key === WHERE_KEY::LESS_THAN_OR_EQUAL)
-//        array_push($whereTemp,  $comparator);
-
-//    if ($key === WHERE_KEY::BETWEEN || $key === WHERE_KEY::NOT_BETWEEN)
-//        if (is_array($comparator))
-//            array_push($whereTemp, $comparator[0], OPERATOR_KEY::AND_KEY, $comparator[1]);
-
-//    if ($key === WHERE_KEY::IN || $key === WHERE_KEY::NOT_IN)
-//        if (is_array($comparator))
-//            array_push($whereTemp, '(', implode(',', $comparator), ')');
-
-//    if ($operator != NULL)
-//        array_push($whereTemp, $operator);
-
-//    self::$whereConditions .= implode(' ', $whereTemp);
-//    return true;
-//}
