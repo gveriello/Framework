126d8eab117bac2ef68841a69302f34fa3f92df5
Giuseppe Veriello <gveriello@outlook.it>
2018-09-23 17:39:27 +0200

    Add FileHelper to create/download file

---
+++ core/utilities/helpers/FileHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+class FileHelper
+{
+
+    public static function createWord($htmlText, $fileName = 'julius_word.doc')
+    {
+        if(!empty($fileName) && !empty($htmlText))
+        {
+            header("Content-type: application/vnd.ms-word");
+            header("Content-Disposition: attachment;Filename=".$fileName);
+            echo $htmlText;
+        }
+    }
+
+    public static function createExcel($htmlTable, $fileName = 'julius_excel.xls')
+    {
+        if(!empty($fileName) && !empty($htmlTable))
+        {
+            header ("Content-Type: application/vnd.ms-excel");
+            header ("Content-Disposition: inline; filename=".$fileName);
+            echo $htmlTable;
+        }
+    }
+
+    public static function createTxt($text, $fileName = 'julius_txt.txt')
+    {
+        if (empty($text))
+            return false;
+
+        file_put_contents($fileName, $text, FILE_APPEND | LOCK_EX);
+        return true;
+    }
+
+    public static function createGenericFile($body, $fileName = 'julius_generic_file', $extension = '.txt')
+    {
+        if (empty($body) || empty($fileName) || empty($extension))
+            return false;
+
+        file_put_contents(basename($fileName).$extension, $body, FILE_APPEND | LOCK_EX);
+        return true;
+    }
+
+}
