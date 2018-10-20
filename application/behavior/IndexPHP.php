e2eda4f5d6df68879e8d188136abd28512a0f15b
Giuseppe Veriello <gveriello@outlook.it>
2018-10-20 15:58:00 +0200

    Implementazione behavior

---
+++ application/behavior/IndexPHP.php
@@ -0,0 +1,23 @@
+<?php
+
+class IndexPHP extends Page implements IEvent
+{
+    public function OnControllerLoaded()
+    {
+    }
+    public function OnLayoutBinded()
+    {
+    }
+    public function OnPreRender()
+    {
+    }
+    public function OnLoad()
+    {
+    }
+    public function CreateTable()
+    {
+    }
+    public function QueryToDB()
+    {
+    }
+}
